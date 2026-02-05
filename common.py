d43c862a5bd7e814bda4c1a39138ac77f3d924e0
fae <git@haku.me>
2026-02-05 11:55:20 +0000

    wrap to printer width.

--- common.py
+++ common.py
@@ -6,20 +6,25 @@ from collections import defaultdict
 
 from markdownify import markdownify
 from rich.console import Console
-from rich.markdown import Markdown
-from rich.markdown import Paragraph
+from rich.console import NewLine
 from rich.markdown import ListElement
 from rich.markdown import ListItem
+from rich.markdown import Markdown
+from rich.markdown import Paragraph
+from rich.segment import Segment
+from rich.segment import Segments
 
 
 class Args:
   def __init__(self):
     parser = argparse.ArgumentParser()
     parser.add_argument("--json",  type=pathlib.Path, required=True)
+    parser.add_argument("--width", type=int, default=56)
     parser.add_argument("--details", action="store_true")
     args = parser.parse_args()
 
     self.json_path = args.json
+    self.text_width = args.width
     self.show_details = args.details
 
 ARGS = Args()
@@ -98,7 +103,11 @@ class Printer:
     Paragraph.new_line = False
     ListElement.new_line = False
     ListItem.new_line = False
-    self.console = Console(highlight=False, no_color=True)
+    self.console = Console(
+        width=ARGS.text_width,
+        no_color=True,
+        highlight=False,
+        )
 
   @staticmethod
   def html_to_md(html):
@@ -109,7 +118,7 @@ class Printer:
     return markdownify(html, strip=['hr'])
 
   def print(self, text):
-    self.console.out(text)
+    self.console.print(text, emoji=False, markup=False)
 
   def print_html(self, html):
     md = self.html_to_md(html)
@@ -120,11 +129,27 @@ class Printer:
   def print_hr(self):
     self.console.print(Markdown('---'))
 
+  def render_item(self, marker, text):
+    opts = self.console.options.update_width(ARGS.text_width - len(marker))
+    lines = self.console.render_lines(text, options=opts, pad=False)
+    ret = []
+
+    ret.append(Segment(marker))
+    ret.extend(lines.pop(0))
+    ret.append(NewLine())
+
+    for l in lines:
+      ret.append(Segment(' ' * len(marker)))
+      ret.extend(l)
+      ret.append(NewLine())
+
+    return Segments(ret)
+
   def print_heading_and_html(self, things):
     for heading, html in things:
       md = self.html_to_md(html)
       if not md:
         continue
       self.console.print(Markdown('---'))
-      self.console.out(heading)
+      self.print(heading)
       self.console.print(Markdown(md))
