ba12e02f68a7909705a5a2afddda030ae2d85288
fae <git@haku.me>
2026-02-07 12:21:21 +0000

    immunities and weaknesses.

--- misc.py
+++ misc.py
@@ -1,8 +1,15 @@
 import re
 
+def com(*arr):
+  return space(*arr, sep=', ')
 
 def space(*arr, sep=" "):
   return sep.join([str(a) for a in arr if a])
+
+def prefix(val, pre):
+  if not val:
+    return ''
+  return f"{pre} {val}"
 
 def level(l):
   if not l['value']:
