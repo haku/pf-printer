5cbc58d43af36f67ea98ad61cf8effc771bd42b8
fae <git@haku.me>
2026-02-24 11:32:53 +0000

    tidy up action icons, add free action.

--- pf_icons.py
+++ pf_icons.py
@@ -1,5 +1,32 @@
 from udchar import Udchars
 
+ACTIONS_FREE = Udchars("free", 9, 24,
+    #0         0         0         0    5
+    "                                    "  #  0
+    "                                    "  #  1
+    "                                    "  #  2
+    "                                    "  #  3
+    "                                    "  #  4
+    "                                    "  #  5
+    "                                    "  #  6
+    "                                    "  #  7
+    "       xx                           "  #  8
+    "      xxxx                          "  #  9
+    "     xx  xx                         "  # 10
+    "    xx    xx                        "  # 11
+    "   xxx     xx                       "  # 12
+    "  xxxxx     xx                      "  # 13
+    " xx  xxx     xx                     "  # 14
+    "xx    xxx     xx                    "  # 15
+    "xx    xxx     xx                    "  # 16
+    " xx  xxx     xx                     "  # 17
+    "  xxxxx     xx                      "  # 18
+    "   xxx     xx                       "  # 19
+    "    xx    xx                        "  # 20
+    "     xx  xx                         "  # 21
+    "      xxxx                          "  # 22
+    "       xx                           "  # 23
+    )
 ACTIONS_1 = Udchars("*   ", 9, 24,
     #0         0         0         0    5
     "                                    "  #  0
@@ -9,23 +36,23 @@ ACTIONS_1 = Udchars("*   ", 9, 24,
     "                                    "  #  4
     "                                    "  #  5
     "                                    "  #  6
-    "       x                            "  #  7
-    "      xxx                           "  #  8
-    "     xxxxx                          "  #  9
-    "    xxxxxxx                         "  # 10
-    "   xxxxxxxxx                        "  # 11
-    "    xxxxxxxxx                       "  # 12
-    " xx  xxxxxxxxx                      "  # 13
-    "xxxx  xxxxxxxxx                     "  # 14
-    "xxxx  xxxxxxxxx                     "  # 15
-    " xx  xxxxxxxxx                      "  # 16
-    "    xxxxxxxxx                       "  # 17
-    "   xxxxxxxxx                        "  # 18
-    "    xxxxxxx                         "  # 19
-    "     xxxxx                          "  # 20
-    "      xxx                           "  # 21
-    "       x                            "  # 22
-    "                                    "  # 23
+    "                                    "  #  7
+    "       xx                           "  #  8
+    "      xxxx                          "  #  9
+    "     xxxxxx                         "  # 10
+    "    xxxxxxxx                        "  # 11
+    "   xxxxxxxxxx                       "  # 12
+    "    xxxxxxxxxx                      "  # 13
+    " xx  xxxxxxxxxx                     "  # 14
+    "xxxx  xxxxxxxxxx                    "  # 15
+    "xxxx  xxxxxxxxxx                    "  # 16
+    " xx  xxxxxxxxxx                     "  # 17
+    "    xxxxxxxxxx                      "  # 18
+    "   xxxxxxxxxx                       "  # 19
+    "    xxxxxxxx                        "  # 20
+    "     xxxxxx                         "  # 21
+    "      xxxx                          "  # 22
+    "       xx                           "  # 23
     )
 ACTIONS_2 = Udchars("**  ", 9, 24,
     #0         0         0         0    5
@@ -36,23 +63,23 @@ ACTIONS_2 = Udchars("**  ", 9, 24,
     "                                    "  #  4
     "                                    "  #  5
     "                                    "  #  6
-    "       x                            "  #  7
-    "      xxx        x                  "  #  8
-    "     xxxxx      xxx                 "  #  9
-    "    xxxxxxx    xxxxx                "  # 10
-    "   xxxxxxxxx  xxxxxxx               "  # 11
-    "    xxxxxxxxx  xxxxxxx              "  # 12
-    " xx  xxxxxxxxx  xxxxxxx             "  # 13
-    "xxxx  xxxxxxxxx  xxxxxxx            "  # 14
-    "xxxx  xxxxxxxxx  xxxxxxx            "  # 15
-    " xx  xxxxxxxxx  xxxxxxx             "  # 16
-    "    xxxxxxxxx  xxxxxxx              "  # 17
-    "   xxxxxxxxx  xxxxxxx               "  # 18
-    "    xxxxxxx    xxxxx                "  # 19
-    "     xxxxx      xxx                 "  # 20
-    "      xxx        x                  "  # 21
-    "       x                            "  # 22
-    "                                    "  # 23
+    "                                    "  #  7
+    "       xx                           "  #  8
+    "      xxxx        xx                "  #  9
+    "     xxxxxx      xxxx               "  # 10
+    "    xxxxxxxx    xxxxxx              "  # 11
+    "   xxxxxxxxxx  xxxxxxxx             "  # 12
+    "    xxxxxxxxxx  xxxxxxxx            "  # 13
+    " xx  xxxxxxxxxx  xxxxxxxx           "  # 14
+    "xxxx  xxxxxxxxxx  xxxxxxxx          "  # 15
+    "xxxx  xxxxxxxxxx  xxxxxxxx          "  # 16
+    " xx  xxxxxxxxxx  xxxxxxxx           "  # 17
+    "    xxxxxxxxxx  xxxxxxxx            "  # 18
+    "   xxxxxxxxxx  xxxxxxxx             "  # 19
+    "    xxxxxxxx    xxxxxx              "  # 20
+    "     xxxxxx      xxxx               "  # 21
+    "      xxxx        xx                "  # 22
+    "       xx                           "  # 23
     )
 ACTIONS_3 = Udchars("*** ", 9, 24,
     #0         0         0         0    5
@@ -63,21 +90,21 @@ ACTIONS_3 = Udchars("*** ", 9, 24,
     "                                    "  #  4
     "                                    "  #  5
     "                                    "  #  6
-    "       x                            "  #  7
-    "      xxx        x                  "  #  8
-    "     xxxxx      xxx       x         "  #  9
-    "    xxxxxxx    xxxxx     xxx        "  # 10
-    "   xxxxxxxxx  xxxxxxx  xxxxxx       "  # 11
-    "    xxxxxxxxx  xxxxxxx  xxxxxx      "  # 12
-    " xx  xxxxxxxxx  xxxxxxx  xxxxxx     "  # 13
-    "xxxx  xxxxxxxxx  xxxxxxx  xxxxxx    "  # 14
-    "xxxx  xxxxxxxxx  xxxxxxx  xxxxxx    "  # 15
-    " xx  xxxxxxxxx  xxxxxxx  xxxxxx     "  # 16
-    "    xxxxxxxxx  xxxxxxx  xxxxxx      "  # 17
-    "   xxxxxxxxx  xxxxxxx  xxxxxx       "  # 18
-    "    xxxxxxx    xxxxx     xxx        "  # 19
-    "     xxxxx      xxx       x         "  # 20
-    "      xxx        x                  "  # 21
-    "       x                            "  # 22
-    "                                    "  # 23
+    "                                    "  #  7
+    "       xx                           "  #  8
+    "      xxxx        xx                "  #  9
+    "     xxxxxx      xxxx        xx     "  # 10
+    "    xxxxxxxx    xxxxxx      xxxx    "  # 11
+    "   xxxxxxxxxx  xxxxxxxx    xxxxxx   "  # 12
+    "    xxxxxxxxxx  xxxxxxxx  xxxxxxxx  "  # 13
+    " xx  xxxxxxxxxx  xxxxxxxx  xxxxxxxx "  # 14
+    "xxxx  xxxxxxxxxx  xxxxxxxx  xxxxxxxx"  # 15
+    "xxxx  xxxxxxxxxx  xxxxxxxx  xxxxxxxx"  # 16
+    " xx  xxxxxxxxxx  xxxxxxxx  xxxxxxxx "  # 17
+    "    xxxxxxxxxx  xxxxxxxx  xxxxxxxx  "  # 18
+    "   xxxxxxxxxx  xxxxxxxx    xxxxxx   "  # 19
+    "    xxxxxxxx    xxxxxx      xxxx    "  # 20
+    "     xxxxxx      xxxx        xx     "  # 21
+    "      xxxx        xx                "  # 22
+    "       xx                           "  # 23
     )
